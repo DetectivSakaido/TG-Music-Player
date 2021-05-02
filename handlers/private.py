78a4b9fa6794635125dbfd23bc1bc4196f05bee5
DetectivSakaido <79519612+DetectivSakaido@users.noreply.github.com>
2021-05-02 15:56:06 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -23,10 +23,10 @@ The commands I currently support are:
             [
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/HiTechRockets"
+                        "Owner", url="https://t.me/AMSuserbot"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/HiTechRocket"
+                        "Channel", url="https://t.me/animemusicstash"
                     )
                 ]
             ]
